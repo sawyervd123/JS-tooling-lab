d453e8dc533a69440997071fb77e989d3973013a
Sawyervd <88403568+Sawyervd@users.noreply.github.com>
2024-07-21 17:43:54 -0400

    initial commit

---
+++ tests/rock_paper_scissors-logic.test.js
@@ -0,0 +1,52 @@
+const { RockPaperScissors } = require('../resources/scripts/rock_paper_scissors.js');
+const mathRandomSpy = jest.spyOn(Math, 'random');
+
+describe('RockPaperScissors class', function () {
+  describe('determineWinner()', function () {
+    test('win cases', function () {
+      const game = new RockPaperScissors('TestUser');
+      expect(game.determineWinner('rock', 'scissors')).toBe('win');
+      expect(game.determineWinner('paper', 'rock')).toBe('win');
+      expect(game.determineWinner('scissors', 'paper')).toBe('win');
+    });
+
+    test('tie cases', function () {
+      const game = new RockPaperScissors('TestUser');
+      expect(game.determineWinner('rock', 'rock')).toBe('tie');
+      expect(game.determineWinner('paper', 'paper')).toBe('tie');
+      expect(game.determineWinner('scissors', 'scissors')).toBe('tie');
+    });
+
+    test('lose cases', function () {
+      const game = new RockPaperScissors('TestUser');
+      expect(game.determineWinner('rock', 'paper')).toBe('lose');
+      expect(game.determineWinner('paper', 'scissors')).toBe('lose');
+      expect(game.determineWinner('scissors', 'rock')).toBe('lose');
+    });
+  });
+
+  describe('generateCPUResponse()', function () {
+    it('Math.Random = 0.1 -> Rock', function() {
+      mathRandomSpy.mockImplementation(() => 0.1);
+      const game = new RockPaperScissors('TestUser');
+      expect(game.generateCPUResponse()).toBe('rock');
+    });
+
+    it('Math.Random = 0.5 -> Paper', function() {
+      mathRandomSpy.mockImplementation(() => 0.5);
+      const game = new RockPaperScissors('TestUser');
+      expect(game.generateCPUResponse()).toBe('paper');
+    });
+
+    it('Math.Random = 0.9 -> Scissors', function() {
+      mathRandomSpy.mockImplementation(() => 0.9);
+      const game = new RockPaperScissors('TestUser');
+      expect(game.generateCPUResponse()).toBe('scissors');
+    });
+  });
+});
+
+// Clean up the spy after all tests are done
+afterAll(() => {
+  mathRandomSpy.mockRestore();
+});
